24b6e1b52ba0e5ed610ed18f0b1915df8b28b0dd
ShangaraevaYaN <yTvqK5qS2025>
2022-03-23 19:31:35 +0300

    Добавлена параметризация к тесту

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize("promo_code", ["0", "1", "2", "3", "4", "5", "6",
+                                        pytest.param("7", marks=pytest.mark.xfail),
+                                        "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_code):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}".format(promo_code)
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_page()
