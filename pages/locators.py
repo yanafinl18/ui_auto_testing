3a697de2fb8fac3b8e222111f0f84c0bc8a69588
ShangaraevaYaN <yTvqK5qS2025>
2022-03-23 23:32:16 +0300

    вынесли 2 метода в BasePage

--- pages/locators.py
+++ pages/locators.py
@@ -1,12 +1,19 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+# class MainPageLocators():
+#     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTERED_FORM = (By.CSS_SELECTOR, "#register_form")
+
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,5 @@
-import time
-
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 
 
@@ -39,3 +38,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     assert page.should_success_message_disappeared(), "Не пропало сообщение"
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
